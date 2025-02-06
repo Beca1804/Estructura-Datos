4a33d0167dfc43d44b2e87e25692dd7a5f0f8a80
Rebeca Garcia <14457@ulsachihuahua.edu.mx>
2025-02-06 10:48:26 -0600

    Agregamos UserData a la carpeta 2

--- UserData.java
+++ UserData.java
@@ -4,16 +4,11 @@
  * Fecha: 8-Febrero-2025
  */
 
-//Crear una nueva clase llamada UserData.java
-//Dentro de esta clase, definir un método llamado fullName.
-//La función debe recibir como parámetros:
-//String firstName (nombre).
-//String lastName (apellido).
-//La función debe retornar un String con el nombre completo usando interpolación.
-//Implementación en la clase principal
-//Crear una instancia de la clase UserData.
-//Llamar a la función fullName y pasarle los parámetros correspondientes.
-//Imprimir el resultado en la consola.
+
+// ponemos la clase publica para que se pueda acceder desde cualquier clase
+// luego creamos un metodo publico para que se pueda acceder desde cualquier clase
+// creamos una variable de tipo string para guardar el nombre completo
+// retornamos el nombre completo con interpolacion (profe)
 
 
 
