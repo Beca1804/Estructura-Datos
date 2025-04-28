50f0a32cbf7da4f504db564c20fb420d056871fd
Rebeca Garcia <14457@ulsachihuahua.edu.mx>
2025-04-28 09:41:42 -0600

    Agregue Ejemplo Cola y Ejemplo Pila

--- ThirdParcial/ThirdParcialMain.java
+++ ThirdParcial/ThirdParcialMain.java
@@ -1,6 +1,27 @@
 package ThirdParcial;
 
 public class ThirdParcialMain {
-    
-    
+    public static void main(String[] args) {
+        DoublyLinkedList list = new DoublyLinkedList();
+
+        list.insert(100);
+        list.insert(200);
+        list.insert(300);
+
+        System.out.println("List forward:");
+        list.displayForward();
+
+        System.out.println("List backward:");
+        list.displayBackward();
+
+        System.out.println("Is 200 in the list? " + list.search(200));
+
+        list.delete(200);
+
+        System.out.println("After deleting 200:");
+        list.displayForward();
+    }
 }
+
+    
+
