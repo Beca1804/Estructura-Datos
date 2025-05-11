2dd7793584bd45e160b37f7f152ab89264b529e2
Rebeca Garcia <14457@ulsachihuahua.edu.mx>
2025-05-10 18:00:45 -0600

    Ejemplo Cola y Ejemplo pila

--- ThirdParcial/EjemploCola.java
+++ ThirdParcial/EjemploCola.java
@@ -4,14 +4,16 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class EjemploCola {
-    public static void main(String[] args) {
-        Queue<String> cola = new LinkedList<>();
+        public  void ColaAdd(String cliente) {
+            Queue<String> cola = new LinkedList<>();
+ 
+// ENQUEUE: Agregando elementos a la cola
 
-        // ENQUEUE: Agregando elementos a la cola
-        cola.add("Cliente 1");
-        cola.add("Cliente 2");
-        cola.add("Cliente 3");
+        cola.add(cliente);
 
+
+    // Funcion para mostar quien esta al frente de la fila
+    
         // PEEK: Consultando quién está al frente de la cola sin eliminarlo
         System.out.println("Cliente al frente: " + cola.peek());
 
@@ -23,5 +25,9 @@ public class EjemploCola {
 
         // PEEK: Consultando quién está ahora al frente
         System.out.println("Cliente al frente ahora: " + cola.peek());
+
     }
-}
+   
+    }
+
+
--- ThirdParcial/ThirdParcialMain.java
+++ ThirdParcial/ThirdParcialMain.java
@@ -1,27 +1,15 @@
 package ThirdParcial;
-
 public class ThirdParcialMain {
-    public static void main(String[] args) {
-        DoublyLinkedList list = new DoublyLinkedList();
+    public static void main (String[] args) {
+        
+       EjemploCola cola = new EjemploCola();
+       cola.ColaAdd("Cliente 1");
+       cola.ColaAdd("Cliente 1");
+       cola.ColaAdd("Cliente 1");
 
-        list.insert(100);
-        list.insert(200);
-        list.insert(300);
-
-        System.out.println("List forward:");
-        list.displayForward();
-
-        System.out.println("List backward:");
-        list.displayBackward();
-
-        System.out.println("Is 200 in the list? " + list.search(200));
-
-        list.delete(200);
-
-        System.out.println("After deleting 200:");
-        list.displayForward();
-    }
+        
+    
 }
-
+}
     
 
